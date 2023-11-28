5b27a9a21571e0b0da7b8190e745a831ada29f41
Jellyfish_Yu <mrbrain699@gmail.com>
2023-11-28 11:25:10 +0800

    modifying deck.cpp

--- Assignment_06(UNO)/Reverse.h
+++ Assignment_06(UNO)/Reverse.h
@@ -3,5 +3,17 @@
 
 // TODO:
 // Define the Reverse class which inherits from ActionCard.
+class Reverse
+{
+private:
+    Card card;
+public:
+    Reverse(Color c);
+    
+};
+
+Reverse::Reverse(Color c) {
+    this->card = Card(c, "reverse");
+}
 
 #endif // REVERSE_H
